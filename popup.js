cd22f3477d2eb3c09f2396565b962d6a0c3ac938
Jessica Chen <j2286che@uwaterloo.ca>
2021-12-26 22:46:03 -0500

    Update manifest.json, popup.html, popup.js, styles.css. Add background.js

--- popup.js
+++ popup.js
@@ -10,5 +10,49 @@ chrome.runtime.onInstalled.addListener (() => {
 console.log('Current Year:', currYear);
 console.log(currDate.toString());
 
+// ON HOVER FUNCTION
 
+window.onload = function() {
+    document.getElementById("B1").onmouseover = function() { hover1() };
+    document.getElementById("B2").onmouseover = function() { hover2() };
+    document.getElementById("B3").onmouseover = function() { hover3() };
+    document.getElementById("B4").onmouseover = function() { hover4() };
+    document.getElementById("B5").onmouseover = function() { hover5() };
+
+    document.getElementById("B1").onmouseout = function() { mouseout() };
+    document.getElementById("B2").onmouseout = function() { mouseout() };
+    document.getElementById("B3").onmouseout = function() { mouseout() };
+    document.getElementById("B4").onmouseout = function() { mouseout() };
+    document.getElementById("B5").onmouseout = function() { mouseout() };
+
+    function hover1() {
+        document.getElementById("plant").src="assets/plant1.png";
+        document.getElementById("mood").innerHTML = "1";
+    }
+
+    function hover2() {
+        document.getElementById("plant").src="assets/plant2.png";
+        document.getElementById("mood").innerHTML = "2";
+    }
+
+    function hover3() {
+        document.getElementById("plant").src="assets/plant3.png";
+        document.getElementById("mood").innerHTML = "3";
+    }
+
+    function hover4() {
+        document.getElementById("plant").src="assets/plant4.png";
+        document.getElementById("mood").innerHTML = "4";
+    }
+
+    function hover5() {
+        document.getElementById("plant").src="assets/plant5.png";
+        document.getElementById("mood").innerHTML = "5";
+    }
+
+    function mouseout() {
+        document.getElementById("plant").src="assets/pot.png";
+        document.getElementById("mood").innerHTML = "&nbsp;";
+    }
+}
 
