0663a2236bd3d94a029c2e2d6dc97050cd70ab76
kevinkim31 <86376921+kevinkim31@users.noreply.github.com>
2021-12-26 22:40:45 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,9 @@
 // DATE
-
-var currDate = new Date();
-var currYear = currDate.getFullYear();
-
-chrome.runtime.onInstalled.addListener (() => {
-    chrome.storage.sync.set({'Year':currYear});
+/*
+chrome.storage.sync.get(['year'], function(result) {
+    var urmom = document.querySelector("#yeartest");
+    urmom.innerHTML = "" + result.year;
 });
-
-console.log('Current Year:', currYear);
-console.log(currDate.toString());
-
+*/
 
 
