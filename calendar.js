dbca5161f5d55e2f73039771af01535c0dccdc31
kevinkim31 <86376921+kevinkim31@users.noreply.github.com>
2021-12-28 22:16:39 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -5,49 +5,45 @@ chrome.storage.sync.get(['year'], function(result) {
     var updateMonth = updateDate.getMonth();
     var updateDay = updateDate.getDate();
 
-    var buttonDay = "day";
+    var isLeap = (updateYear%400 == 0)||(updateYear%100 != 0 && updateYear%4 == 0);
 
-    for (let count = 1; count <= 31; count++)
+
+    if (!isLeap)
     {
-        if (updateDay == count)
-        {
-            buttonDay = buttonDay + count; 
-        }
+        document.querySelector("#FEB29").style.visibility= "hidden";
     }
 
-    var tableDay = document.getElementById("calendarTable").rows.namedItem(buttonDay).cells;
+    chrome.storage.sync.get(['calendar'], function(result3) {
 
-    chrome.storage.sync.get(['currButton'], function(result1) {
+        var calUpdate = result3.calendar;
 
-        if (result1.currButton == 1)
+        for (let month = 0; month < calUpdate.length; month++)
         {
-            tableDay[updateMonth+1].getElementsByTagName("Button")[0].style.background = "#9dc36c";
-            
-            /*
-            var milf = document.querySelector("#hoverMood");
-            var dilf = document.querySelector("hoverDate");
+            for (let day = 0; day < calUpdate[month].length; day++)
+            {
+                var tableDay = document.getElementById("calendarTable").rows.namedItem("day"+(day+1)).cells;
 
-            milf.innerHTML = "Fantastic";
-            var hi = tableDay[updateMonth+1].getElementsByTagName("Button")[0].id;
-            dilf.innerHTML = "" + hi;
-            */
-
-        }
-        else if (result1.currButton == 2)
-        {
-            tableDay[updateMonth+1].getElementsByTagName("Button")[0].style.background = "#76a53d";
-        }
-        else if (result1.currButton == 3)
-        {
-            tableDay[updateMonth+1].getElementsByTagName("Button")[0].style.background = "#537926";
-        }
-        else if (result1.currButton == 4)
-        {
-            tableDay[updateMonth+1].getElementsByTagName("Button")[0].style.background = "#3b4723";
-        }
-        else if (result1.currButton == 5)
-        {
-            tableDay[updateMonth+1].getElementsByTagName("Button")[0].style.background = "#242714";
+                if (calUpdate[month][day] == 1)
+                {
+                    tableDay[month+1].getElementsByTagName("Button")[0].style.background = "#9dc36c";
+                }
+                else if (calUpdate[month][day] == 2)
+                {
+                    tableDay[month+1].getElementsByTagName("Button")[0].style.background = "#76a53d";
+                }
+                else if (calUpdate[month][day] == 3)
+                {
+                    tableDay[month+1].getElementsByTagName("Button")[0].style.background = "#537926";
+                }
+                else if (calUpdate[month][day] == 4)
+                {
+                    tableDay[month+1].getElementsByTagName("Button")[0].style.background = "#3b4723";
+                }
+                else if (calUpdate[month][day] == 5)
+                {
+                    tableDay[month+1].getElementsByTagName("Button")[0].style.background = "#242714";
+                }
+            }
         }
     });
 
