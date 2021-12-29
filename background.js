76fbc7900ab5581d409dc5bb2922f35c90808c79
kevinkim31 <86376921+kevinkim31@users.noreply.github.com>
2021-12-28 22:15:44 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,4 +3,22 @@ chrome.runtime.onInstalled.addListener (() => {
     var currDate = new Date();
     var currYear = currDate.getFullYear();
     chrome.storage.sync.set({year:currYear});
+    const year = new Array(12);
+
+    year[0] = new Array(31);
+    year[1] = new Array(29);
+    year[2] = new Array(31);
+    year[3] = new Array(30);
+    year[4] = new Array(31);
+    year[5] = new Array(30);
+    year[6] = new Array(31);
+    year[7] = new Array(31);
+    year[8] = new Array(30);
+    year[9] = new Array(31);
+    year[10] = new Array(30);
+    year[11] = new Array(31);
+
+    chrome.storage.sync.set({calendar:year});
+
+   
 });
