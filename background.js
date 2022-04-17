108470e33fa64f84c61eab0f51be43c9c31d3f24
Daniel Sun <shuyangdanielsun@gmail.com>
2022-04-17 03:31:30 -0400

    Many changes
    1. added a function to print changes in the database to the console.
    2. no longer refreshes data everytime chrome is updated.
    3. shortened some lines of code

--- background.js
+++ background.js
@@ -1,27 +1,47 @@
 // the year structure is created and stored in chrome storage on install
 // this prevents the complete erasure of the array by accident
 
-chrome.runtime.onInstalled.addListener (() => {
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "instal") {
+        var currDate = new Date();
+        var currYear = currDate.getFullYear();
 
-    var currDate = new Date();
-    var currYear = currDate.getFullYear();
+        chrome.storage.sync.set({
+            year: currYear
+        });
 
-    chrome.storage.sync.set({year:currYear});
+        const year = new Array(12);
 
-    const year = new Array(12);
+        year[0] = new Array(31);
+        year[1] = new Array(29);
+        year[2] = new Array(31);
+        year[3] = new Array(30);
+        year[4] = new Array(31);
+        year[5] = new Array(30);
+        year[6] = new Array(31);
+        year[7] = new Array(31);
+        year[8] = new Array(30);
+        year[9] = new Array(31);
+        year[10] = new Array(30);
+        year[11] = new Array(31);
 
-    year[0] = new Array(31);
-    year[1] = new Array(29);
-    year[2] = new Array(31);
-    year[3] = new Array(30);
-    year[4] = new Array(31);
-    year[5] = new Array(30);
-    year[6] = new Array(31);
-    year[7] = new Array(31);
-    year[8] = new Array(30);
-    year[9] = new Array(31);
-    year[10] = new Array(30);
-    year[11] = new Array(31);
+        chrome.storage.sync.set({
+            calendar: year
+        });
+    } else if (details.reason == "update") {
+        // to be added if we ever update again
+    }
 
-    chrome.storage.sync.set({calendar:year});
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, {
+            oldValue,
+            newValue
+        }] of Object.entries(changes)) {
+        console.log(
+            `Storage key "${key}" in namespace "${namespace}" changed.`,
+            `Old value was "${oldValue}", new value is "${newValue}".`
+        );
+    }
 });
--- popup.js
+++ popup.js
@@ -1,178 +1,98 @@
-chrome.storage.sync.get(['year'], function(e) {
+// buttons that display the mood and corresponding picture when hovered
+window.onload = function () {
 
-    // static variables
-    var currentYear = e.year;
-
+    // 
     var updateDate = new Date();
     var updateMonth = updateDate.getMonth();
     var updateDay = updateDate.getDate();
     var updateYear = updateDate.getFullYear();
 
-    // creates a new array if stored year does not match the system's current year
-    // this new array then replaces the one in chrome storage
-    if (updateYear != currentYear)
-    {
-        chrome.storage.sync.set({year:updateYear});
-        const year = new Array(12);
-        var isLeap = (updateYear%400 == 0)||(updateYear%100 != 0 && updateYear%4 == 0);
+    chrome.storage.sync.get(['year'], function (e) {
 
-        year[0] = new Array(31);
+        // static variables
+        var currentYear = e.year;
 
-        if (isLeap)
-        {
-            year[1] = new Array(29);
+        // creates a new array if stored year does not match the system's current year
+        // this new array then replaces the one in chrome storage
+        if (updateYear != currentYear) {
+            chrome.storage.sync.set({
+                year: updateYear
+            });
+            const year = new Array(12);
+            var isLeap = (updateYear % 400 == 0) || (updateYear % 100 != 0 && updateYear % 4 == 0);
+
+            year[0] = new Array(31);
+
+            if (isLeap) {
+                year[1] = new Array(29);
+            } else {
+                year[1] = new Array(28);
+            }
+
+            year[2] = new Array(31);
+            year[3] = new Array(30);
+            year[4] = new Array(31);
+            year[5] = new Array(30);
+            year[6] = new Array(31);
+            year[7] = new Array(31);
+            year[8] = new Array(30);
+            year[9] = new Array(31);
+            year[10] = new Array(30);
+            year[11] = new Array(31);
+
+            // stores the current year in chrome storage
+            chrome.storage.sync.set({
+                calendar: year
+            });
         }
-        else
-        {
-            year[1] = new Array(28);
-        }
-
-        year[2] = new Array(31);
-        year[3] = new Array(30);
-        year[4] = new Array(31);
-        year[5] = new Array(30);
-        year[6] = new Array(31);
-        year[7] = new Array(31);
-        year[8] = new Array(30);
-        year[9] = new Array(31);
-        year[10] = new Array(30);
-        year[11] = new Array(31);
-
-        // stores the current year in chrome storage
-        chrome.storage.sync.set({calendar:year});
-    }
+    });
 
     // if user presses first button, mood is set to fantastic
     // stored array is then called from chrome storage and updated with this mood
-    document.querySelector("#B1").addEventListener('click', ()=> {
+    for (let i = 1; i <= 5; i++) {
+        document.querySelector("#B" + i).addEventListener('click', () => {
 
-        chrome.storage.sync.set({currButton:1});
-        window.location.href = 'calendar.html';
-        chrome.storage.sync.get(['calendar'], function(f) {
+            chrome.storage.sync.set({
+                currButton: i
+            });
 
-            var calUpdate = f.calendar;
-            calUpdate[updateMonth][updateDay - 1] = 1;
+            window.location.href = 'calendar.html';
+            chrome.storage.sync.get(['calendar'], function (f) {
 
-            chrome.storage.sync.set({calendar:calUpdate});
+                var calUpdate = f.calendar;
+                calUpdate[updateMonth][updateDay - 1] = i;
 
-        });
+                chrome.storage.sync.set({
+                    calendar: calUpdate
+                }, function () {
 
-    });
-    
-    // if user presses second button, mood is set to fine
-    // stored array is then called from chrome storage and updated with this mood
-    document.querySelector("#B2").addEventListener('click', ()=> {
-        chrome.storage.sync.set({currButton:2});
-        window.location.href = 'calendar.html';
-        chrome.storage.sync.get(['calendar'], function(f) {
-
-            var calUpdate = f.calendar;
-            calUpdate[updateMonth][updateDay - 1] = 2;
-
-            chrome.storage.sync.set({calendar:calUpdate});
-
-        });
-        
-    });
-    
-    // if user presses third button, mood is set to bored
-    // stored array is then called from chrome storage and updated with this mood
-    document.querySelector("#B3").addEventListener('click', ()=> {
-        chrome.storage.sync.set({currButton:3});
-        window.location.href = 'calendar.html';
-        chrome.storage.sync.get(['calendar'], function(f) {
-
-            var calUpdate = f.calendar;
-            calUpdate[updateMonth][updateDay - 1] = 3;
-
-            chrome.storage.sync.set({calendar:calUpdate});
+                });
+            });
 
         });
 
-    });
-    
-    // if user presses fourth button, mood is set to upset
-    // stored array is then called from chrome storage and updated with this mood
-    document.querySelector("#B4").addEventListener('click', ()=> {
-        chrome.storage.sync.set({currButton:4});
-        window.location.href = 'calendar.html';
-        chrome.storage.sync.get(['calendar'], function(result2) {
+        document.getElementById("B" + i).onmouseover = function () {
+            hover(i);
+        };
 
-            var calUpdate = result2.calendar;
-            calUpdate[updateMonth][updateDay - 1] = 4;
-
-            chrome.storage.sync.set({calendar:calUpdate});
-
-        });
-
-    });
-    
-    // if user presses fifth button, mood is set to miserable
-    // stored array is then called from chrome storage and updated with this mood
-    document.querySelector("#B5").addEventListener('click', ()=> {
-        chrome.storage.sync.set({currButton:5});
-        window.location.href = 'calendar.html';
-        chrome.storage.sync.get(['calendar'], function(result2) {
-
-            var calUpdate = result2.calendar;
-            calUpdate[updateMonth][updateDay-1]=5;
-
-            chrome.storage.sync.set({calendar: calUpdate});
-
-        });
-
-    });
+        document.getElementById("B" + i).onmouseout = function () {
+            mouseout();
+        };
+    }
 
     // if user presses the calendar button, they are sent to the calendar page with no changes made to the mood
-    document.querySelector("#calendarbutton").addEventListener('click', ()=> {
-
+    document.querySelector("#calendarbutton").addEventListener('click', () => {
         window.location.href = 'calendar.html';
-
     });
-});   
 
-// buttons that display the mood and corresponding picture when hovered
-window.onload = function() {
-
-    document.getElementById("B1").onmouseover = function() { hover1() };
-    document.getElementById("B2").onmouseover = function() { hover2() };
-    document.getElementById("B3").onmouseover = function() { hover3() };
-    document.getElementById("B4").onmouseover = function() { hover4() };
-    document.getElementById("B5").onmouseover = function() { hover5() };
-
-    document.getElementById("B1").onmouseout = function() { mouseout() };
-    document.getElementById("B2").onmouseout = function() { mouseout() };
-    document.getElementById("B3").onmouseout = function() { mouseout() };
-    document.getElementById("B4").onmouseout = function() { mouseout() };
-    document.getElementById("B5").onmouseout = function() { mouseout() };
-
-    function hover1() {
-        document.getElementById("plant").src="assets/plant1.png";
-        document.getElementById("mood").innerHTML = "fantastic";
-    }
-
-    function hover2() {
-        document.getElementById("plant").src="assets/plant2.png";
-        document.getElementById("mood").innerHTML = "fine";
-    }
-
-    function hover3() {
-        document.getElementById("plant").src="assets/plant3.png";
-        document.getElementById("mood").innerHTML = "bored";
-    }
-
-    function hover4() {
-        document.getElementById("plant").src="assets/plant4.png";
-        document.getElementById("mood").innerHTML = "upset";
-    }
-
-    function hover5() {
-        document.getElementById("plant").src="assets/plant5.png";
-        document.getElementById("mood").innerHTML = "miserable";
+    function hover(mood) {
+        var moods = ["fantastic", "fine", "bored", "upset", "miserable"]
+        document.getElementById("plant").src = "assets/plant" + mood + ".png";
+        document.getElementById("mood").innerHTML = moods[mood - 1];
     }
 
     function mouseout() {
-        document.getElementById("plant").src="assets/pot.png";
+        document.getElementById("plant").src = "assets/pot.png";
         document.getElementById("mood").innerHTML = "&nbsp;";
     }
 }
